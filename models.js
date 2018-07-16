5e95aaa20852c39b8471e87cb6b606397863f461
Joel Burton <joel@joelburton.com>
2018-07-16 14:15:46 -0700

    Fix which .save method was missing.

--- models.js
+++ models.js
@@ -67,22 +67,6 @@ class Reservation {
 
     return new Reservation(results.row[0]);
   }
-
-  async save() {
-    if (this.id === undefined) {
-      const result = await db.query(
-        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
-          VALUES ($1, $2, $3, $4)
-          RETURNING id`,
-        [this.customerId, this.numGuests, this.startAt, this.notes])
-      this.id = result.rows[0].id;
-    } else {
-      await db.query(
-        `UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3
-           WHERE id=$4`,
-        [this.numGuests, this.startAt, this.notes, this.id])
-    }
-  }
 }
 
 class Customer {
@@ -136,6 +120,22 @@ class Customer {
     return await Reservation.getReservationsForCustomer(this.id);
   }
 
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers (first_name, last_name, phone, notes)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id`,
+        [this.firstName, this.lastName, this.phone, this.notes]);
+      console.log("result", result.rows[0].id);
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4)
+           WHERE id=$5`,
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]);
+    }
+  }
 }
 
 module.exports = { Customer, Reservation };
